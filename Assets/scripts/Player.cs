6b57636d1d6c6f91ecd5c45145b883598cc2bb5f
RPC29 <rpc29.c@gmail.com>
2025-05-20 15:51:43 +0530

    kinda works
    controls suck ass but at least it works

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject mesh;
+    public GameObject cam;
+    public GameObject directionMarker;
     public LayerMask groundLayer;
     SphereCollider coll;
     Rigidbody rb;
@@ -20,15 +22,24 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
-        AlignKart();
-        input = Input.GetAxis("Horizontal");
-        rot = mesh.transform.rotation;
-        rot.y = input*0.5f;
-        mesh.transform.rotation = rot;
+        //AlignKart();
+        input += 0.025f * Input.GetAxisRaw("Horizontal");
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.5) input += (input > -0.1f) ? -0.1f : (input < 0.1f) ? 0.1f : 0f;
+        input = Mathf.Clamp(input, -5, 5);
+        //mesh.transform.localRotation = Quaternion.Euler(0, input * 20f, 0);
+        Debug.Log(rb.velocity.x + ", " + rb.velocity.y + ", " + rb.velocity.z);
+        if (rb.velocity.magnitude > 0.1f)mesh.transform.rotation = Quaternion.LookRotation(rb.velocity);// + new Vector3(0, input * 20f, 0);
+        if (rb.velocity.magnitude > 0.1f)mesh.transform.rotation = Quaternion.Euler(0, mesh.transform.rotation.eulerAngles.y + input * 20f, 0);// + new Vector3(0, input * 20f, 0);
+        if (rb.velocity.magnitude > 0.1f)cam.transform.rotation = Quaternion.LookRotation(rb.velocity);// + new Vector3(0, input * 20f, 0);
+        if (rb.velocity.magnitude > 0.1f)cam.transform.rotation = Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0);// + new Vector3(0, input * 20f, 0);
+        //mesh.transform.rotation = Quaternion.Euler(0, mesh.transform.rotation.eulerAngles.y, 0);
         if (Input.GetButton("Fire1"))
         {
-            print("shoot" + input);
-            rb.AddForce(-mesh.transform.forward * spe, ForceMode.Acceleration);
+            rb.AddForce(mesh.transform.forward * spe);
+        }
+        if (Input.GetButton("Fire2"))
+        {
+            rb.AddForce(-mesh.transform.forward * spe);
         }
     }
 
