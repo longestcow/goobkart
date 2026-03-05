ce69ae41ff881b8031c959a2cd61dc130f7c5519
RPC29 <rpc29.c@gmail.com>
2026-03-05 19:18:33 +0530

    Bike

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -3,60 +3,141 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    [Header("Kart Stats")]
+    public float spe = 10; //forward speed (10 is default)
+    public float eps = -5; //reverse speed (-5 is default)
+    public float rotspeed = 20; //speed of turning (20 is default)
+    public float weight = 1; //weight of kart (1 is default)
+    public float stylemultiplier = 1; //amount of style points earnt per style thing (not implemented yet)
+    public float boostmultiplier = 1; //the amount of boost the kart gets from boosting on boost panels (not implemented yet)
+
+
+
+    [Header("Everything else")]
     public GameObject mesh;
+    public GameObject parentobject;
+    public GameObject fakecam;
     public GameObject cam;
-    public GameObject directionMarker;
     public LayerMask groundLayer;
+    public Text Speedometer;
+    public Transform wheel1;
+    public Transform wheel2;
+    public Transform frontthing;
+    public Transform cyclemain;
+    
     SphereCollider coll;
     Rigidbody rb;
     float input = 0f;
+    float rotinput = 0f;
+    float driftdir;
+    bool driftstart;
+    bool drifting;
     Quaternion rot;
-    public float spe = 10;
+    bool isgrounded;
     void Start()
     {
         coll = GetComponent<SphereCollider>();
         rb = GetComponent<Rigidbody>();
+        mesh.transform.parent = parentobject.transform;
+        rb.mass = weight;
     }
-    void Update()
+    void FixedUpdate()
     {
-        //AlignKart();
-        input += 0.025f * Input.GetAxisRaw("Horizontal");
-        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.5) input += (input > -0.1f) ? -0.1f : (input < 0.1f) ? 0.1f : 0f;
-        input = Mathf.Clamp(input, -5, 5);
-        //mesh.transform.localRotation = Quaternion.Euler(0, input * 20f, 0);
-        Debug.Log(rb.velocity.x + ", " + rb.velocity.y + ", " + rb.velocity.z);
-        if (rb.velocity.magnitude > 0.1f)mesh.transform.rotation = Quaternion.LookRotation(rb.velocity);// + new Vector3(0, input * 20f, 0);
-        if (rb.velocity.magnitude > 0.1f)mesh.transform.rotation = Quaternion.Euler(0, mesh.transform.rotation.eulerAngles.y + input * 20f, 0);// + new Vector3(0, input * 20f, 0);
-        if (rb.velocity.magnitude > 0.1f)cam.transform.rotation = Quaternion.LookRotation(rb.velocity);// + new Vector3(0, input * 20f, 0);
-        if (rb.velocity.magnitude > 0.1f)cam.transform.rotation = Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0);// + new Vector3(0, input * 20f, 0);
-        //mesh.transform.rotation = Quaternion.Euler(0, mesh.transform.rotation.eulerAngles.y, 0);
+        rb.AddForce(mesh.transform.forward * input, ForceMode.Force);
+        wheel2.localRotation = Quaternion.Euler(0,0,wheel2.localRotation.eulerAngles.z + rb.velocity.magnitude);
+        wheel1.localRotation = Quaternion.Euler(0,0,wheel1.localRotation.eulerAngles.z + rb.velocity.magnitude);
+        frontthing.transform.localRotation = Quaternion.Lerp(frontthing.transform.localRotation,Quaternion.Euler(0,rotinput*100,0),0.1f);
+        cyclemain.localRotation = Quaternion.Euler(0.3f * ((Vector3.SignedAngle(mesh.transform.forward,rb.velocity,mesh.transform.up))),270,0);
+    }
+
+    private void LateUpdate()
+    {
+
+        cam.transform.position = mesh.transform.position;
+        fakecam.transform.position = mesh.transform.position;
+        if (rb.velocity.magnitude > 0.1f) fakecam.transform.rotation = Quaternion.LookRotation(rb.velocity);// + new Vector3(0, input * 20f, 0);
+        if (rb.velocity.magnitude > 0.1f) fakecam.transform.rotation = Quaternion.Euler(0, fakecam.transform.rotation.eulerAngles.y + driftdir * 45, 0);// + new Vector3(0, input * 20f, 0);
+        cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, fakecam.transform.rotation, 0.05f);
+
+    }
+
+    private void Update()
+    {
+        AlignKart();
+
+        if (Input.GetButton("Drift") && !drifting && isgrounded && rb.velocity.magnitude > 0.5f)
+        {
+            driftstart = true;
+        }
+        if (driftstart)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0.1f)
+            {
+                driftdir = 1;
+                driftstart = false;
+                drifting = true;
+            }
+            if (Input.GetAxisRaw("Horizontal") < -0.1f)
+            {
+                driftdir = -1;
+                driftstart = false;
+                drifting = true;
+            }
+            
+        }
+        if (!Input.GetButton("Drift"))
+        {
+            driftdir = 0;
+            drifting = false;
+            driftstart = false;
+        }
+
+        rotinput = (driftdir + Input.GetAxisRaw("Horizontal"))/2f;
+        if (!isgrounded) rotinput *= 0.3f;
+
+        mesh.transform.Rotate(0, rotinput * Time.deltaTime * rotspeed * rb.velocity.magnitude ,0, Space.World);
+
+
+        mesh.transform.position = this.transform.position;
+
         if (Input.GetButton("Fire1"))
         {
-            rb.AddForce(mesh.transform.forward * spe);
+            input = spe;
         }
-        if (Input.GetButton("Fire2"))
+        else if (Input.GetButton("Fire2"))
         {
-            rb.AddForce(-mesh.transform.forward * spe);
+            input = eps;
         }
+        else
+        {
+            input = 0;
+        }
+
+        Speedometer.text = ((int)(20f * rb.velocity.magnitude)).ToString();
     }
 
 
     void AlignKart(){
-        Ray ray = new Ray(transform.position, Vector3.down);
+        Ray ray = new Ray(mesh.transform.position, -mesh.transform.up);
         RaycastHit info;
+
+        isgrounded = Physics.Raycast(ray, out info, groundLayer);
 
         if (Physics.Raycast(ray, out info, 2f, groundLayer))
         {
             // mesh.transform.rotation = Quaternion.Lerp(mesh.transform.rotation, 
-            // Quaternion.FromToRotation(Vector3.up, info.normal),
+            mesh.transform.rotation = Quaternion.Lerp(mesh.transform.rotation,Quaternion.FromToRotation(mesh.transform.up, info.normal) * mesh.transform.rotation,0.01f);
+
+
             // Time.deltaTime * 10f);
-            rot = Quaternion.FromToRotation(Vector3.up, info.normal);
-            rot.z = mesh.transform.rotation.z;
-            rot.y = mesh.transform.rotation.y;
-            mesh.transform.rotation = rot;
+            //rot = Quaternion.FromToRotation(Vector3.up, info.normal);
+            //rot.z = mesh.transform.rotation.z;
+            //rot.y = mesh.transform.rotation.y;
+            //mesh.transform.rotation = rot;
         }
     }
 }
